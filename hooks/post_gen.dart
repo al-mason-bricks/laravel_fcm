d18c13e7f604f5ba7dd7d21f93522b5bb497c433
Loai Dev <loaidev64@gmail.com>
2023-11-12 14:52:11 +0200

    adding FCM_KEY to .env file

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -5,4 +5,5 @@ void run(HookContext context) {
       .warn('You need to add HasFirebaseTokens trait to app/Models/User model');
   context.logger.warn('You need to run this command:');
   context.logger.warn('composer require kutia-software-company/larafirebase');
+  context.logger.warn('don\'t forget to add FCM_KEY to your .env file');
 }
